1efd95a5671de423462cf8152888ccbad00d5f4d
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2022-02-13 14:57:14 +0200

    delete not use files

--- src/packriver/routes.py
+++ src/packriver/routes.py
@@ -5,19 +5,3 @@ from flask import (render_template, request, redirect, url_for)
 @app.route('/home')
 def index():
     return render_template("index.html")
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-@app.route('/article')
-def article():
-    return render_template("create-article.html")
-
-@app.route('/create', methods=("GET", "POST"))
-def create_article():
-    if request.method == "POST":
-        title = request.form["title"]
-        text = request.form["text"] 
-        intro = request.form["intro"] 
-        return redirect(url_for("article"))
